e9aec7b2f7ce8828216ec660e6767b5fec4d5884
geoskl <gskliasnl@gmail.com>
2018-01-17 05:35:52 +0100

    bootstrap and login form

--- sw_lifecycle/src/App.js
+++ sw_lifecycle/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import './sw_lifecycle.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="full-form">
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Username</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username"/>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
